8a27501c5d826f6b468aa5b8c2ab3b973a693b95
pradoxal_IV <68543395+adrienchanove@users.noreply.github.com>
2022-01-31 11:51:13 +0100

    création entitee

---
+++ src/Controller/CharacterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CharacterController extends AbstractController
+{
+    #[Route('/character', name: 'character')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CharacterController.php',
+        ]);
+    }
+
+    #[Route('/character/display', name: 'displayCharacter')]
+    public function display(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CharacterController.php',
+        ]);
+    }
+}
